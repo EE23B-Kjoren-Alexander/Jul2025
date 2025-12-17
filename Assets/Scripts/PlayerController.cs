60b2b8b19f517a79d10733ed650d6eb702fef1aa
EE23B-Kjoren-Alexander <alexander.kjoren@elev.ga.ntig.se>
2025-12-17 08:46:37 +0100

    Jamen,Hejsan

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,13 @@ public class PlayerController : MonoBehaviour
 Vector2 movement = Vector2.zero;
 [SerializeField]
 float speed = 3;
+     
+Animator anim;     
+     
      void Start()
     {
-        
+        //variablen anim har datatypen Animator
+        anim = GetComponent<Animator>();
     }
     void Update()
     {
@@ -21,5 +25,18 @@ float speed = 3;
         Vector2 vector = value.Get<Vector2>();
         movement = Vector2.one * vector;
         print(vector);
+
+        if(movement.magnitude == 0)
+        {
+            anim.Play("Idle");
+        }
+        else if(movement.y < 0)
+        {
+            anim.Play("WalkDown");
+        }
+        else if(movement.y > 0)
+        {
+            anim.Play("WalkUp");
+        }
     }
 }
